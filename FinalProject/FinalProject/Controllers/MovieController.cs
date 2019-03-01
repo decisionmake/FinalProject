fb6849fc757da9064e3c2724e4cad9228a48d513
Jamil Abbas <jamilabbas787@gmail.com>
2019-02-28 20:10:53 -0500

    Added API information

--- FinalProject/FinalProject/Controllers/MovieController.cs
+++ FinalProject/FinalProject/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,9 +17,13 @@ namespace FinalProject.Controllers
 
         public ActionResult Genre()
         {
-            
+            var client = new RestClient("https://api.themoviedb.org/3/discover/movie?with_genres=27&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=d4d54b8d7ddedcc20679758413820443");
+            var request = new RestRequest(Method.GET);
+            request.AddParameter("undefined", "{}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            response.Content.First();
 
-            return View();
+            return View(response);
         }
 
         public ActionResult Popular()
