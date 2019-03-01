1a4e68c1f78122e90869c5fbc3f8c8fd7170130d
Jamil Abbas <jamilabbas787@gmail.com>
2019-02-28 19:11:12 -0500

    Created Movie Controller, View For Movie-Controller (index), add some editing to view

---
+++ FinalProject/FinalProject/Controllers/MovieController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Genre()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Popular()
+        {
+
+            return View();
+        }
+
+    }
+}
