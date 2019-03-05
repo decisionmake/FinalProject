63a60d093c2b724730c38bf456ba2f028016f91b
ba7399 <ba7399@gmail.com>
2019-03-04 21:01:40 -0500

    Genre throughput to selection completed

--- FinalProject/FinalProject/Models/GenreSelection/GenreMovieModel.cs
+++ FinalProject/FinalProject/Models/GenreSelection/GenreMovieModel.cs
@@ -8,23 +8,7 @@ namespace FinalProject.Models
 {
     public class GenreMovieModel
     {
-        //public class Result
-        //{
-        //    public int vote_count { get; set; }
-        //    public int id { get; set; }
-        //    public bool video { get; set; }
-        //    public double vote_average { get; set; }
-        //    public string title { get; set; }
-        //    public double popularity { get; set; }
-        //    public string poster_path { get; set; }
-        //    public string original_language { get; set; }
-        //    public string original_title { get; set; }
-        //    public List<int> genre_ids { get; set; }
-        //    public string backdrop_path { get; set; }
-        //    public bool adult { get; set; }
-        //    public string overview { get; set; }
-        //    public string release_date { get; set; }
-        //}
+        
 
 
         public class Genre
@@ -32,16 +16,5 @@ namespace FinalProject.Models
             public int id { get; set; }
             public string name { get; set; }
         }
-
-       
-        public class RootObject
-        {
-            public int page { get; set; }
-            public int total_results { get; set; }
-            public int total_pages { get; set; }
-            //public List<Result> results { get; set; }
-            public List<Genre> genres { get; set; }
-        }
-
     }
 }
