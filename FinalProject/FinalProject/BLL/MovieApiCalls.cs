3817f2ea6bfc493769fda30060f711a4ccc125a4
Jamil Abbas <jamilabbas787@gmail.com>
2019-03-04 21:03:00 -0500

    Created MovieApiCalls Class to handle objects and api

---
+++ FinalProject/FinalProject/BLL/MovieApiCalls.cs
@@ -0,0 +1,36 @@
+﻿using FinalProject.Models.MoviePopularity;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProject.BLL
+{
+    public class MovieApiCalls
+    {
+
+        public static MoviePopularityViewModel GetPopularMovies()
+        {
+            var client = new RestClient("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=d4d54b8d7ddedcc20679758413820443");
+            var request = new RestRequest(Method.GET);
+            request.AddParameter("undefined", "{}", ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+
+            var jsonResults = JsonConvert.DeserializeObject<MoviePopularityRoot>(response.Content);
+
+            int[] randomNumber = RandomNumberGenerator.GetNumber(jsonResults.results.Count());
+            MoviePopularityViewModel movies = new MoviePopularityViewModel
+            {
+                MovieOne = jsonResults.results[randomNumber[0]],
+                MovieTwo = jsonResults.results[randomNumber[1]]
+            };
+
+            return movies;
+
+        }
+
+
+    }
+}
--- FinalProject/FinalProject/Controllers/MovieController.cs
+++ FinalProject/FinalProject/Controllers/MovieController.cs
@@ -52,25 +52,22 @@ namespace FinalProject.Controllers
 
         public ActionResult Popular()
         {
-            var client = new RestClient("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=d4d54b8d7ddedcc20679758413820443");
-            var request = new RestRequest(Method.GET);
-            request.AddParameter("undefined", "{}", ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
+            //var client = new RestClient("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=d4d54b8d7ddedcc20679758413820443");
+            //var request = new RestRequest(Method.GET);
+            //request.AddParameter("undefined", "{}", ParameterType.RequestBody);
+            //IRestResponse response = client.Execute(request);
+
+            //var jsonResults = JsonConvert.DeserializeObject<MoviePopularityRoot>(response.Content);
 
 
-            var deserial = new JsonDeserializer();
+            //int[] randomNumber = RandomNumberGenerator.GetNumber(jsonResults.results.Count());
+            //MoviePopularityViewModel movies = new MoviePopularityViewModel
+            //{
+            //    MovieOne = jsonResults.results[randomNumber[0]],
+            //    MovieTwo = jsonResults.results[randomNumber[1]]
+            //};
 
-            var jsonResults = JsonConvert.DeserializeObject<MoviePopularityRoot>(response.Content);
-
-
-            int[] randomNumber = RandomNumberGenerator.GetNumber(jsonResults.results.Count());
-            MoviePopularityViewModel movies = new MoviePopularityViewModel
-            {
-                MovieOne = jsonResults.results[randomNumber[0]],
-                MovieTwo = jsonResults.results[randomNumber[1]]
-            };
-
-            return View(movies);
+            return View(MovieApiCalls.GetPopularMovies());
         }
 
     }
