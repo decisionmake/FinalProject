83b44b6f910ce6f3611407c1906e309de7b81b9e
ba7399 <ba7399@gmail.com>
2019-03-07 17:02:04 -0500

    scratch yelp api call

---
+++ FinalProject/FinalProject/Models/FoodModels/FoodViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Yelp.Api.Models;
+
+namespace FinalProject.Models.FoodModels
+{
+    public class FoodViewModel
+    {
+        public BusinessResponse BusinessOne { get; set; }
+        public BusinessResponse BusinessTwo { get; set; }
+
+
+    }
+}
