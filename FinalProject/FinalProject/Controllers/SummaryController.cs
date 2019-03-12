b81ca083a31f3eeb4a29230c5577bdd81072f87a
Jamil Abbas <jamilabbas787@gmail.com>
2019-03-11 21:12:08 -0400

    Dependency Injection for summary controller

--- FinalProject/FinalProject/Controllers/SummaryController.cs
+++ FinalProject/FinalProject/Controllers/SummaryController.cs
@@ -4,6 +4,7 @@ using FinalProject.Models.DAL_Objects;
 using FinalProject.Models.GenreSelection;
 using FinalProject.Models.MoviePopularity;
 using FinalProject.Models.SummaryPage;
+using FinalProject.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,15 +17,21 @@ namespace FinalProject.Controllers
 {
     public class SummaryController : Controller
     {
-        
-        private MovieVotingHistoryDbContext db = new MovieVotingHistoryDbContext();
+
+        //private MovieVotingHistoryDbContext db = new MovieVotingHistoryDbContext();
+
+        private readonly ISummaryService _service;
+
+        public SummaryController(ISummaryService service)
+        {
+            _service = service;
+        }
 
         // GET: Summary
         public ActionResult Index(SummaryInformation summary)
         {
-            
-            DecisionLogger.EditMovieDeciosionTracker(summary.MovieTitle, summary.id, summary.PosterPath, db);
-            DecisionLogger.IndicisionTracker(db);
+            _service.TrackMovie(summary.MovieTitle, summary.id, summary.PosterPath, _service.db());
+            _service.TrackIndecision(_service.db());
 
             return View(summary);
         }
---
+++ FinalProject/FinalProject/Services/SummaryService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FinalProject.DAL;
+using FinalProject.DAL.InformationTracking;
+
+namespace FinalProject.Services
+{
+    public class SummaryService : ISummaryService
+    {
+        public MovieVotingHistoryDbContext db()
+        {
+            MovieVotingHistoryDbContext db = new MovieVotingHistoryDbContext();
+
+            return db;
+        }
+
+        public void TrackIndecision(MovieVotingHistoryDbContext db)
+        {
+            DecisionLogger decisionLogger = new DecisionLogger();
+            decisionLogger.IndicisionTracker(db);
+
+        }
+
+        public void TrackMovie(string movieTitle, int id, string posterPath, MovieVotingHistoryDbContext db)
+        {
+            DecisionLogger decisionLogger = new DecisionLogger();
+            decisionLogger.EditMovieDeciosionTracker(movieTitle, id, posterPath, db);
+
+        }
+    }
+}
