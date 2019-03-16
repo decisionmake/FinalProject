380905dd405338a596943f67bcb17d3dd2a3cea0
Jamil Abbas <jamilabbas787@gmail.com>
2019-03-16 13:30:34 -0400

    Create Project for test

---
+++ FinalProject/FinalProjectTest.Tests/RandomNumberGeneratorTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FinalProjectTest.Tests
+{
+    [TestClass]
+    public class RandomNumberGeneratorTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
